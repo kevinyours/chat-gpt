8f5fc863a0ec51a717c9636c632c11152e7d88e3
Kevin You <eilgwon88@gmail.com>
2023-05-07 13:33:01 +0900

    refactor: middleware

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,15 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-const validatePath = (pathname: string) => {
-  return {
-    isRoom: pathname.startsWith('/room'),
-    isChat: pathname.startsWith('/chat'),
-    isConversation: pathname.startsWith('/conversation'),
-  };
-};
-
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
-  const { isRoom, isChat, isConversation } = validatePath(
-    request.nextUrl.pathname
-  );
 
-  if (isRoom || isChat || isConversation) {
-    const hasApiKey = request.cookies.get('apiKey');
-    if (!hasApiKey) return NextResponse.rewrite(new URL('/', request.url));
-  }
+  const hasApiKey = request.cookies.get('apiKey');
+  if (!hasApiKey) return NextResponse.rewrite(new URL('/', request.url));
 
   return response;
 }
 
 export const config = {
-  matcher: '/:path*',
+  matcher: ['/rooms/:path*', '/chat/:path*'],
 };
